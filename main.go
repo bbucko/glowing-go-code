09428d8010dd12bd57a9324b9c39246afdae0a7d
Blazej Bucko <blazej.bucko@gmail.com>
2014-05-17 18:17:10 +0200

    Some tests

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
 import "fmt"
+import "github.com/dgryski/dkeyczar"
 
 func main() {
 	fmt.Printf("Hello, world.\n")
+
+	plaintext := []byte("hello world")
+
+	reader := dkeyczar.NewFileReader("./")
+	crypter, _ := dkeyczar.NewCrypter(reader)
+
+	ciphertext, _ := crypter.Encrypt(plaintext)
+
+	fmt.Print(ciphertext)
 }
